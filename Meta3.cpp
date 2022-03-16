72cea65ceab5b8bae1a96aa48c10257c442ed397
JoaoESmoreira <jesmoreira2010@gmail.com>
2022-03-16 00:52:59 +0000

    Balanced

--- Meta3.cpp
+++ Meta3.cpp
@@ -9,8 +9,10 @@ class AVL {
         struct Node *left;
         struct Node *right;
 
-        Node() : m_user_name(0), left(nullptr), right(nullptr) {}
-        Node(std::string user_name, int credit_card, int date) : m_user_name(user_name), left(nullptr), right(nullptr) {
+        int m_height;
+
+        Node() : m_user_name(0), left(nullptr), right(nullptr), m_height(0) {}
+        Node(std::string user_name, int credit_card, int date) : m_user_name(user_name), left(nullptr), right(nullptr), m_height(1) {
             m_credit_card[credit_card] = date;
         }
     };
@@ -25,8 +27,80 @@ class AVL {
         }
     }
 
+    int m_node_height(Node *node) {
+        int a, b;
+
+        if (node->left == nullptr) {
+            a = 0;
+        } else {
+            a = node->left->m_height;
+        }
+        if (node->right == nullptr) {
+            b = 0;
+        } else {
+            b = node->right->m_height;
+        }
+
+
+        if (a < b) {
+            return 1 + b;
+        } else {
+            return 1 + a;
+        }
+    }
+
+    Node* m_right_rotate(Node *node) {
+        Node *left_son  = node->left;
+        Node *grand_son = left_son->right;
+    
+        // Perform rotation
+        left_son->right = node;
+        node->left      = grand_son;
+    
+        // Update heights
+        node->m_height     = m_node_height(node);
+        left_son->m_height = m_node_height(left_son);
+
+        return left_son;
+    }
+
+    Node* m_left_rotate(Node *node) {
+        Node *right_son = node->right;
+        Node *grand_son = right_son->left;
+    
+        // Perform rotation
+        right_son->left = node;
+        node->right = grand_son;
+    
+        // Update heights
+        node->m_height = m_node_height(node);
+        right_son->m_height = m_node_height(right_son);
+    
+        return right_son;
+    }
+
+    int m_balance(Node *node) {
+        int a, b;
+        if (node == nullptr)
+            return 0;
+
+        if (node->left == nullptr) {
+            a = 0;
+        } else {
+            a = node->left->m_height;
+        }
+        if (node->right == nullptr) {
+            b = 0;
+        } else {
+            b = node->right->m_height;
+        }
+        return a - b;
+    }
+
+
     Node* m_insert(Node *node, std::string user_name, int credit_card, int date) {
 
+        // Basic insertion
         if (node == nullptr) {
             node = new Node(user_name, credit_card, date);
             std::cout << "NOVO UTILIZADOR CRIADO\n";
@@ -44,6 +118,28 @@ class AVL {
                 std::cout << "NOVO CARTAO INSERIDO\n";
             }
         }
+
+        // Update height
+        node->m_height = m_node_height(node);
+
+        // Balance all tree
+        int balance = m_balance(node);
+        if (balance > 1 && user_name < node->left->m_user_name) {
+            // left letf
+            return m_right_rotate(node);
+        } if (balance < -1 && user_name > node->right->m_user_name) {
+            // right right
+            return m_left_rotate(node);
+        } if (balance > 1 && user_name > node->left->m_user_name) {
+            // left right
+            node->left = m_left_rotate(node->left);
+            return m_right_rotate(node);
+        } if (balance < -1 && user_name < node->right->m_user_name) {
+            // right left
+            node->right = m_right_rotate(node->right);
+            return m_left_rotate(node);
+        }
+
         return node;
     }
 

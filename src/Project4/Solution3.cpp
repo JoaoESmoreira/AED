ba890b1eb72a638fe1ec423948e75c93e48cbe0c
JoaoESmoreira <jesmoreira2010@gmail.com>
2022-05-03 16:05:38 +0100

    quicksort implemented

---
+++ src/Project4/Solution3.cpp
@@ -0,0 +1,132 @@
+
+#include <iostream>
+#include <vector>
+
+
+void read_raster (const int &M, std::vector<int> &raster) {
+    int input;
+
+    for (int i = 0; i < M; ++i) {
+        std::cin >> input;
+        raster.push_back(input);
+    }
+}
+
+void quick_sort (std::vector<int> &raster, int start, int end) {
+
+    if (end - start < 1)
+        return;
+
+    int pivot = start;
+    int i = end;
+    int j = start;
+
+    while (i != j) {
+
+        for (; i > j; --i) {
+            if (raster[pivot] >= raster[i]) {
+                std::swap(raster[pivot], raster[i]);
+                pivot = i;
+                j++;
+                break;
+            }
+        }
+
+        for (; j < i; ++j) {
+            if (raster[j] >= raster[pivot]) {
+                std::swap(raster[pivot], raster[j]);
+                pivot = j;
+                i--;
+                break;
+            }
+        }
+    }
+
+    quick_sort(raster, start, i - 1);
+    quick_sort(raster, i + 1, end);
+
+}
+
+void print_raster (std::vector<int> &raster) {
+    for (int i = 0; i < (int) raster.size(); ++i) {
+        std::cout << raster[i] << " ";
+    } 
+    std::cout << "\n";
+}
+
+int amplitude (std::vector<int> &raster) {
+    return raster[raster.size() - 1] - raster[0];
+}
+
+int mediana (std::vector<int> raster) {
+    int size_0 = raster.size() - 1;
+
+    if ((size_0 + 1) % 2 == 0) {
+        return (raster[size_0 / 2] + raster[(size_0 / 2) + 1]) / 2;
+    } 
+    return raster[((size_0) / 2)];
+}
+    
+int percentil (const std::vector<int> &raster, const int &value) {
+    float count = 0;
+
+    for (int i = 0; i < (int) raster.size(); ++i) {
+        if (raster[i] < value)
+            count++;
+        else
+            return (count / raster.size()) * 100;
+    }
+    return (count / raster.size()) * 100;
+}
+
+
+
+int main() {
+
+    std::vector<int> raster;
+    std::string command;
+    int N, M;
+    std::cout << "\n";
+
+    do {
+        std::cin >> command;
+
+        if (command.compare("RASTER") == 0) {
+            // read array
+            std::cin >> N >> M;
+            read_raster(N*M, raster);
+            std::cout << "RASTER GUARDADO\n";
+            quick_sort(raster, 0, N*M - 1);
+
+        } else if (command.compare("AMPLITUDE") == 0) {
+
+            std::cout << amplitude(raster) << "\n";
+
+        } else if (command.compare("MEDIANA") == 0) {
+
+            std::cout << mediana(raster) << "\n";
+
+        } else if (command.compare("PERCENTIL") == 0) {
+            int t, input, output;
+
+            std::cin >> t;
+            for (int i = 1; i <= t; ++i) {
+                std::cin >> input;
+
+                output = percentil(raster, input);
+
+                std::cout << output;
+                if (i == t)
+                    std::cout << "\n";
+                else 
+                    std::cout << " ";
+            }
+        }
+
+    } while (command.compare("TCHAU"));
+
+    print_raster(raster);
+
+
+    return 0;
+}
